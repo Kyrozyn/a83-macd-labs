f4043d128d4cccf7bf2b2f2cae333267a827cece
Andreas Rizki Rizaldi <arizkirizaldi@email.unikom.ac.id>
2019-04-09 13:42:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "databaseky.database.windows.net";
+    $user = "kyrozyn";
+    $pass = "anyand32";
+    $db = "database1";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
